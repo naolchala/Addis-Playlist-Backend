debd0249be01d23fb472b208a993b52307aecc5c
Naol Chala <naolchala6@gmail.com>
2023-03-22 17:21:56 +0300

    🤦‍♂️ COOOOOOORS

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	async rewrites() {
+		return [
+			{
+				source: "/api/:path*",
+				destination: "https://addis-playlist-backend.vercel.app/:path*",
+			},
+		];
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

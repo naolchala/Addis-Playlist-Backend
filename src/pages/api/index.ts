4abb43fe283fb85faab00a2ba9958419119c403b
Naol Chala <naolchala6@gmail.com>
2023-03-22 17:31:24 +0300

    🙏 Cors Fixed: https://vercel.com/guides/how-to-enable-cors

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -1,16 +1,23 @@
+import helmet from "helmet";
 import { NextApiRequest, NextApiResponse } from "next";
+import nextConnect from "next-connect";
+
+const route = nextConnect({
+	onNoMatch: (res, req) => (res.statusCode = 404),
+});
 
 type TestResponse = {
 	message: string;
 	time: Date;
 };
 
-export default function handler(
-	req: NextApiRequest,
-	res: NextApiResponse<TestResponse>
-) {
+route.use(helmet());
+
+route.get((req: NextApiRequest, res: NextApiResponse) => {
 	res.status(200).json({
 		message: "API Working..",
 		time: new Date(),
 	});
-}
+});
+
+export default route;

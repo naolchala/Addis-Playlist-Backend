cc7429eb932bda41698d75dd226ffa4884f83670
Naol Chala <naolchala6@gmail.com>
2023-04-03 11:55:16 +0300

    deleting Loading spinner

--- src/lib/utils/prisma.ts
+++ src/lib/utils/prisma.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 const prismaGlobal = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
-	prismaGlobal.prisma || new PrismaClient({ log: ["query", "warn"] });
+	prismaGlobal.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
 	prismaGlobal.prisma = prisma;
